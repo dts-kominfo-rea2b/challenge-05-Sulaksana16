c9d0dd619f8a9b06620887310f6d2d9e1976a6b7
Sulaksana16 <sulaksana16@gmail.com>
2022-06-04 20:12:43 +0700

    solve challenge

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (datas, fn) => {
+  const sortedList = fn(datas)
+  return sortedList.map((lists, index) => `${index + 1}. ${lists}`)
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = datas => datas.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = datas => datas.reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
